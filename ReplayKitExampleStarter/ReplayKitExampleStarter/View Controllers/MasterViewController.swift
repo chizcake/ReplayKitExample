26865835e967a72fbbdd0871a1f65151f0ef29fd
JUNYEONG.YOO <woody.chiz@gmail.com>
2017-03-11 01:23:18 +0900

    Add ReplayKitExampleStarter

---
+++ ReplayKitExampleStarter/ReplayKitExampleStarter/View Controllers/MasterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.swift
+//  ReplayKitExampleStarter
+//
+//  Created by JUNYEONG.YOO on 3/11/17.
+//  Copyright © 2017 chizcake. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		
+	}
+
+}
